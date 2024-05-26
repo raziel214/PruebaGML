c88a65b3a2dd77d7da5469b1a40a9cf62f28a6b7
raziel214 <soulreavers214@gmail.com>
2024-05-25 19:40:31 -0500

    feat: añadir modelo, entidad y estructura de la aplicación
    - Añadido el modelo de datos para la entidad Cliente.
    - Configurada la estructura de la aplicación con las capas de dominio, aplicación e infraestructura.
    - Implementadas las clases básicas y los repositorios.

--- clientes-api/src/main/java/com/quimbaya/clientes_api/ClientesApiApplication.java
+++ clientes-api/src/main/java/com/quimbaya/clientes_api/ClientesApiApplication.java
@@ -2,8 +2,14 @@ package com.quimbaya.clientes_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.quimbaya.clientes_api")
+@ComponentScan(basePackages = "com.quimbaya.clientes_api")
+@EnableJpaRepositories(basePackages = {"com.quimbaya.clientes_api"})
+@EntityScan(basePackages = {"com.quimbaya.clientes_api"})
 public class ClientesApiApplication {
 
 	public static void main(String[] args) {
